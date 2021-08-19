387343ec7589a1f2f2d898d1793243c7e4d6c252
jesus <a01731627@itesm.mx>
2021-08-19 11:43:27 -0500

    variables 2

---
+++ variables/var.js
@@ -0,0 +1,21 @@
+var nombre = "jesus"
+var email = "a01731627@itesm.mx"
+var edad = 22
+function getNombre(){
+    console.log(nombre);
+    var myNameElement = document.getElementById("nombre");
+    myNameElement.innerHTML = nombre;
+}
+
+function getNombre(){
+    console.log(email);
+    var myEmailElement = document.getElementById("email");
+    myEmailElement.innerHTML = '<b>' + email + '</b>';
+}
+
+function getNombre(){
+    console.log(nombre);
+    var myNameElement = document.getElementById("nombre");
+    myNameElement.innerHTML = nombre;
+    
+}
